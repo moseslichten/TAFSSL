4a49270693e460f8db301af7ea7a7b394c09459d
Leonid Karlinsky <leokarlin@users.noreply.github.com>
2020-09-26 10:51:29 +0300

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,9 @@
+# --------------------------------------------------------
+# TAFSSL
+# Copyright (c) 2019 IBM Corp
+# Licensed under The Apache-2.0 License [see LICENSE for details]
+# --------------------------------------------------------
+
 from .ResNet import *
 from .DenseNet import *
 # from .DenseNetGradCAM import *
